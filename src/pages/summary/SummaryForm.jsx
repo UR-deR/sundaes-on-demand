8b118a29921cba01b7661097e4ab1ca093597499
axtx4869 <shota.furuno@claves.co.jp>
2021-12-22 13:04:59 +0900

    Test and code summary form

---
+++ src/pages/summary/SummaryForm.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+export const SummaryForm = () => {
+  const [tcChecked, setTcChecked] = useState(false);
+
+  return (
+    <Form>
+      <Form.Group controlId="terms-and-conditions">
+        <Form.Check
+          type="checkbox"
+          checked={tcChecked}
+          onChange={(e) => setTcChecked(e.target.checked)}
+          label={
+            <span>
+              I agree to <span style={{ color: 'blue' }}>Terms and Conditions</span>
+            </span>
+          }
+        />
+      </Form.Group>
+      <Button variant="primary" type="submit" disabled={!tcChecked}>
+        Confirm order
+      </Button>
+    </Form>
+  );
+};
---
+++ src/pages/summary/test/SummaryForm.test.jsx
@@ -0,0 +1,23 @@
+import { screen, render, fireEvent } from '@testing-library/react';
+import { SummaryForm } from '../SummaryForm';
+
+test('initial conditions', () => {
+  render(<SummaryForm />);
+  const checkbox = screen.getByRole('checkbox', { name: 'I agree to Terms and Conditions' });
+  expect(checkbox).not.toBeChecked();
+
+  const confirmButton = screen.getByRole('button', { name: 'Confirm order' });
+  expect(confirmButton).toBeDisabled();
+});
+
+test('Checkbox enables button on first click and disables on second click', () => {
+  render(<SummaryForm />);
+  const checkbox = screen.getByRole('checkbox', { name: 'I agree to Terms and Conditions' });
+  const confirmButton = screen.getByRole('button', { name: 'Confirm order' });
+
+  fireEvent.click(checkbox);
+  expect(confirmButton).toBeEnabled();
+
+  fireEvent.click(checkbox);
+  expect(confirmButton).toBeDisabled();
+});
